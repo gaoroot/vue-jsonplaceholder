4b71aa908b5009d29577e130eecc16da81c7c69b
Alexander Govorin <govorinao@gmail.com>
2023-12-18 07:37:15 +0000

    fix

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,14 +1,5 @@
 <script setup>
-// import { RouterLink } from 'vue-router'
-// import { storeToRefs } from 'pinia'
-// import { useCommentStore } from '../stores/todo'
-
 defineProps(['todo'])
-
-// const { getPostComments } = storeToRefs(useCommentStore())
-// const { fetchComments } = useCommentStore()
-
-// fetchComments()
 </script>
 
 <template>
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,17 +1,13 @@
 <script setup>
 import { useRoute } from 'vue-router'
 import { storeToRefs } from 'pinia'
-// import { useAuthorStore } from '../stores/author'
 import { useTodoStore } from '../stores/todo'
 import Todo from '../components/Todo.vue'
 
 const route = useRoute()
-// const { getPostAuthor } = storeToRefs(useAuthorStore())
-// const { fetchAuthors } = useAuthorStore()
 const { todo, loading, error } = storeToRefs(useTodoStore())
 const { fetchTodo } = useTodoStore()
 
-// fetchAuthors()
 fetchTodo(route.params.id)
 </script>
 
